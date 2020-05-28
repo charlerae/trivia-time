e58d47263e6bd42d6d5c86fd091925b49d8c5d8a
charlerae <contactcharlerae@gmail.com>
2020-05-27 20:54:38 -0400

    refactors the getClue function of callback game version

--- 01-callback-style.js
+++ 01-callback-style.js
@@ -5,15 +5,12 @@ export function getClue(callback) {
     request.addEventListener('readystatechange', () => {
         if (request.readyState !== XMLHttpRequest.DONE) return;
         if (request.status < 200 || request.status >= 300) {
-            return;
+            callback(request.status);
         } else {
-            return callback(request.status);
+            const data = JSON.parse(request.responseText)
+            callback(null, data);
         };
 
-        const data = JSON.parse(request.responseText);
-        console.log(data);
-
-        callback(null, data);
     })
 
     request.open('GET', 'https://jservice.xyz/api/random-clue');
@@ -22,20 +19,3 @@ export function getClue(callback) {
 
 
 
-
-// exports = getClue();
-
-
-
-
-// const xmlReq = new XMLHttpRequest();
-// xmlReq.addEventListener('readystatechange', () => {
-//     if (xmlReq.readyState !== XMLHttpRequest.DONE) return;
-//     if (xmlReq.status < 200 || xmlReq.status >= 300) return;
-
-//     const data = JSON.parse(xmlReq.responseText);
-//     console.log(data);
-// });
-
-// xmlReq.open('GET', 'https://api.weather.gov/points/39.7456,-97.0892');
-// xmlReq.send();
